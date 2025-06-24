6d1f94fbc89a9be7465a26111c023c8c00784b5d
PavanKumar161911 <pavankumarkasimkota1611@gmail.com>
2025-06-24 15:26:53 +0530

    Add files via upload

---
+++ Supplementry/src/main/java/Project/Supplementry/Userdetails/Transaction.java
@@ -0,0 +1,80 @@
+package Project.Supplementry.Userdetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Transaction")
+public class Transaction {
+    
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="txn-id")
+	private int txnid;
+	@Column(name="date")
+	private int date;
+	@Column(name="txnamount")
+	private int txnamount;
+	@Column(name="paymentmethod")
+	private String paymentmethod;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="bank_id")
+	private BankAccount bankAccount;
+	
+	public BankAccount getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+	public Userdetails getUser() {
+		return user;
+	}
+	public void setUser(Userdetails user) {
+		this.user = user;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	 private Userdetails user;
+
+	
+	
+	
+	
+	
+	public int getTxnid() {
+		return txnid;
+	}
+	public void setTxnid(int txnid) {
+		this.txnid = txnid;
+	}
+	public int getDate() {
+		return date;
+	}
+	public void setDate(int date) {
+		this.date = date;
+	}
+	public int getTxnamount() {
+		return txnamount;
+	}
+	public void setTxnamount(int txnamount) {
+		this.txnamount = txnamount;
+	}
+	public String getPaymentmethod() {
+		return paymentmethod;
+	}
+	public void setPaymentmethod(String paymentmethod) {
+		this.paymentmethod = paymentmethod;
+	}
+	
+}
